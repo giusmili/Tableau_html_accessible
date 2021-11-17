9d9d72f85461c5c5881fc3099f32702f7ac8faa3
giusmili <giusmili67@gmail.com>
2021-11-17 10:09:25 +0100

    add new template

--- js/app.js
+++ js/app.js
@@ -40,26 +40,72 @@ document.addEventListener("DOMContentLoaded",()=>{
         },
     ]
     */
+
     let stat = [
         {
-            classement : 1,
-            langage : "Python",
-            Type : "App web",
-            Score : "100%"
+            unite : "1 octet",
+            abbreviation : "Aucune",
+            taille : 1,
+            exemple : "4 octets : Un caractère au format utf-8"
         },
 
         {
-            classement : 2,
-            langage : "Java",
-            Type : "App web",
-            Score : "95%"
-            
+            unite : "1 kilooctet",
+            abbreviation : "ko",
+            taille : "10<sup>3</sup>",
+            exemple : "20 ko : Mots prononcés par une personne, par jour"
+        },
+        {
+            unite : "1 megaoctet",
+            abbreviation : "Mo",
+            taille : "10<sup>8</sup>",
+            exemple : "3 Mo : Texte d'une édition quotidienne du New York Times"
+        },
+        {
+            unite : "1 gigaoctet",
+            abbreviation : "Go",
+            taille : "10<sup>9</sup>",
+            exemple : "1 Go : Mots prononcés par une personne durant une vie"
+        },
+        {
+            unite : "1 téraoctet",
+            abbreviation : "To",
+            taille : "10<sup>12</sup>",
+            exemple : "93 To : Mots prononcés par l'ensemble de la population mondiale, par jour"
+        },
+        {
+            unite : "1 pétaoctet",
+            abbreviation : "Po",
+            taille : "10<sup>15</sup>",
+            exemple : "4 Po : Données générées par Facebook, par jour"
+        },
+        {
+            unite : "1 exaoctet",
+            abbreviation : "Eo",
+            taille : "10<sup>18</sup>",
+            exemple : "605 Eo : Données générées par le téléscope SKA, par jour"
+        },
+        {
+            unite : "1 zetaoctet",
+            abbreviation : "Zo",
+            taille : "10<sup>21</sup>",
+            exemple : "40 Zo : Données générées par le web en 2020, par an"
         }
+
     ]
     /* test table */
-    for( data of stat){
-        console.log("Classement "+data.classement+"\n langage "+data.langage+"\n type "+data.Type+"\n score "+data.Score)
+    let elements = document.querySelector("tbody")
+    for(data of stat){
+        console.log("unité "+data.unite+"\n abbréviation "+data.abbreviation+"\n taille "+data.taille+"\n exemple "+data.exemple)
+        elements.innerHTML+=`<tr>
+            <td>${data.unite}</td>
+            <td>${data.abbreviation}</td>
+            <td>${data.taille}</td>
+            <td>${data.exemple}</td>
+        </tr>  
+        `
     }
     console.table(stat)
+    console.log(typeof stat)
         
 })
